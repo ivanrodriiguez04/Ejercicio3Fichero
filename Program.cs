4b278b62070ef432da2a42a40879a31d3fb9e1ce
csi22-irodhan <ivanrodriiguez04@gmail.com>
2024-03-19 14:28:04 +0100

    19/03/2024

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ejercicio3Fichero 
+{
+    /// <summary>
+    /// Clase princicpal de la aplicacion
+    /// irodhan -> 19/03/2024
+    /// </summary>
+    class Program 
+    { 
+        /// <summary>
+        /// Metodo principal de la aplicacion
+        /// irodhan -> 19/03/2024
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) 
+        {
+            string fichero = "C:\\Users\\csi22-irodhan\\Desktop\\ficheroPrueba.txt";
+            Console.WriteLine("En que linea deseas escribir: ");
+            int numeroLinea=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Que desea escribir: ");
+            string nuevoTexto=Console.ReadLine();
+            try
+            {
+                string[] lineas=File.ReadAllLines(fichero);
+
+                if (numeroLinea >= 1 && numeroLinea <= fichero.Length)
+                {
+                    lineas[numeroLinea - 1] = nuevoTexto;
+
+                    File.WriteAllLines(fichero, lineas);
+                }
+                else 
+                {
+                    Console.WriteLine("La linea en la que desea escribir esta fuera de rango");
+                }
+            }
+            catch (IOException e) 
+            { 
+                Console.WriteLine("Error al leer o escribir en el archivo: "+e.Message);
+            }
+        }
+    }
+}
